a34d79985f4cb7a54a84cf363299c4bfdd136a71
Jp <josephpalafos24@gmail.com>
2023-07-13 21:32:22 -0500

    updated html

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const mapSpace =document.getElementById('mapspace')
+const mapSpace =document.getElementById('map-container');
+ 
 getApi2()
 
-function getApi(){    
+function getApi(){  
+preventDefault();  
 requestUrl =  'https://api.musixmatch.com/ws/1.1/chart.artists.gethttps://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=3&country=itapikey=14780e106eef4c8cc8559fc275070950'
 fetch(requestUrl)
         .then(function (response) {
@@ -11,17 +13,16 @@ fetch(requestUrl)
             console.log(data)
     });}
 
-    function getApi2(){
+function getApi2(){
     
-        requestUrl =  'https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&format=png&zoom=12&size=400x400&key=AIzaSyB3UbSsi87BYPQG_akKhzUbuUIpc5xWhZs'
+requestUrl =  'https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&format=png&zoom=12&size=400x400&key=AIzaSyB3UbSsi87BYPQG_akKhzUbuUIpc5xWhZs'
         
-        fetch(requestUrl)
-                .then(function (response) {
-                  console.log(response)
-                  map= document.createElement('image')
-                  map.setAttribute('src',response.url)
-                  console.log(map)
-                  mapSpace.append(map)
-                })
- 
-                  ;}
+  fetch(requestUrl)
+      .then(function (response) {
+          console.log(response)
+           map= document.createElement('img')
+           map.setAttribute('src',response.url)
+           console.log(map)
+           mapSpace.append(map)
+          })
+  ;}
