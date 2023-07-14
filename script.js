b7b430c504758be7245db1495b3338bc1219b8e7
Sean <sdrowan93@gmail.com>
2023-07-13 21:16:41 -0500

    get artists array

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const mapSpace =document.getElementById('mapspace')
+const mapSpace =document.getElementById('map-container')
+const countrySearch = document.getElementById('cityname')
+const searchButton = document.getElementById('search-button')
+console.log(countrySearch.value)
 getApi2()
+let obj ={ 'united Kingdon': 'uk'}
 
+//http://api.musixmatch.com/ws/1.1/track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc
 function getApi(){    
-requestUrl =  'https://api.musixmatch.com/ws/1.1/chart.artists.gethttps://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=3&country=itapikey=14780e106eef4c8cc8559fc275070950'
+  event.preventDefault()
+  let country = countrySearch.value
+requestUrl = ('https://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=10&country='+ country +'&apikey=14780e106eef4c8cc8559fc275070950')
 fetch(requestUrl)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
             console.log(data)
+            console.log(countrySearch.value)
+           
     });}
 
     function getApi2(){
@@ -18,10 +27,12 @@ fetch(requestUrl)
         fetch(requestUrl)
                 .then(function (response) {
                   console.log(response)
-                  map= document.createElement('image')
+                  map = document.createElement('img')
                   map.setAttribute('src',response.url)
+              
                   console.log(map)
                   mapSpace.append(map)
                 })
  
                   ;}
+    searchButton.addEventListener('click',getApi)              
